283a2366225cfdfd11d569ded15dc9cd1ba52f9f
aldia_mechdream <futami16237@gmail.com>
2026-04-21 17:20:35 +0800

    添加 presetCategory 常量以改进类别映射逻辑

--- lib/runner.ts
+++ lib/runner.ts
@@ -1,12 +1,19 @@
 import { basename } from 'node:path';
 import { describe, it } from 'vitest';
 import { log } from './log.js';
+const presetCategory: Record<string, string> = {
+  mem: 'memory',
+};
 
 const kt = (filename: string, fn: () => any) => {
-  const [category, tag] = basename(filename)
-    .replace(/test.ts$/g, '')
+  let [category, tag] = basename(filename)
+    .replace(/.test.ts$/g, '')
     .split('_');
+
+  category = presetCategory[category] ?? category;
+
   let args: any = null;
+
   describe(category, () =>
     it(tag, () => {
       args = fn();
