26950dfeee04fb9c96a60d9cf0f313b9d97a2f0f
aldia_mechdream <futami16237@gmail.com>
2026-04-21 17:18:09 +0800

    重构 kt 函数以使用 basename 处理文件名，并改进日志记录逻辑

--- lib/runner.ts
+++ lib/runner.ts
@@ -1,10 +1,22 @@
+import { basename } from 'node:path';
 import { describe, it } from 'vitest';
 import { log } from './log.js';
 
 const kt = (filename: string, fn: () => any) => {
-  const [category, tag] = filename.replace(/test.ts$/g, '').split('_');
+  const [category, tag] = basename(filename)
+    .replace(/test.ts$/g, '')
+    .split('_');
   let args: any = null;
-  describe(category, () => it(tag, () => (args = fn())));
-  log(category, tag, args);
+  describe(category, () =>
+    it(tag, () => {
+      args = fn();
+      if (typeof args?.then === 'function') {
+        args.then((v: any) => log(category, tag, v));
+      } else {
+        log(category, tag, args);
+      }
+      return args;
+    }),
+  );
 };
 Reflect.set(globalThis, 'kt', kt);
