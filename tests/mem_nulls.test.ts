6350e0fb5203050638380e002cc62b3916ec6590
aldia_mechdream <futami16237@gmail.com>
2026-04-21 17:27:20 +0800

    全放null和全放undefined是一样大的

---
+++ tests/mem_nulls.test.ts
@@ -0,0 +1,19 @@
+kt(import.meta.filename, () => {
+  const arrays = [];
+
+  // 记录内存使用情况
+  const beforeMemory = process.memoryUsage().heapUsed;
+
+  // 创建10万个空数组
+  for (let i = 0; i < ITER_COUNT; i++) {
+    arrays.push(null);
+  }
+
+  // 记录内存使用情况
+  const afterMemory = process.memoryUsage().heapUsed;
+
+  // 计算内存差异
+  const memoryUsed = afterMemory - beforeMemory;
+
+  return `全放null数组长度：${ITER_COUNT}: ${memoryUsed / MB} MB`;
+});
