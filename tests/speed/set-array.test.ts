c818e46219bb723822727be9aae2971ab32f14bd
louie <futami16237@gmail.com>
2026-04-22 21:09:52 +0800

    通过ITER_RATIO调整数值

--- tests/speed/set-array.test.ts
+++ tests/speed/set-array.test.ts
@@ -2,6 +2,8 @@ kt(import.meta.filename, () => {
   const set = new Set();
   const array = [];
 
+  ITER_RATIO = 0.01;
+
   for (let i = 0; i < 12; i++) {
     // 预先准备好一些数据，防止后面没处删
     array.push(i);
@@ -10,17 +12,17 @@ kt(import.meta.filename, () => {
 
   let i = 0;
   setInterval(() => {
-    console.log(i / (ITER_COUNT / 100));
+    console.log(i / ITER_COUNT);
   }, 1000);
   const s0 = performance.now();
-  for (i = 0; i < ITER_COUNT / 100; i++) {
+  for (i = 0; i < ITER_COUNT; i++) {
     set.add(i); // 测试 add
     set.has(i); // 测试 has
     set.delete(i); // 测试 delete
   }
   const s1 = performance.now();
 
-  for (i = 0; i < ITER_COUNT / 100; i++) {
+  for (i = 0; i < ITER_COUNT; i++) {
     array.push(i); // 测试 push
     array.includes(i); // 测试 includes
     array.splice(i, 1); // 测试 splice
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vitest/config';
+import replace from '@rollup/plugin-replace';
 
 export default defineConfig({
   test: {
     setupFiles: ['./lib/setup.ts', './lib/log.ts', './lib/runner.ts'],
   },
+  plugins: [
+    replace({
+      'kt(import.meta.filename, () => {': 'kt(import.meta.filename, () => {ITER_RATIO = 1;',
+    }),
+  ],
 });
