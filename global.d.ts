9b5abd1bd25be761bd73b9c1569170ef5512092d
louie <futami16237@gmail.com>
2026-04-22 21:05:38 +0800

    重构全局常量定义：将 ITER_COUNT_BASE 和 ITER_RATIO 的设置移至 setup.ts，并更新 ITER_COUNT 的计算方式

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,18 @@
+/**
+ * `1e6` by default
+ */
+declare const ITER_COUNT_BASE: number;
+
+/**
+ * @returns `ITER_COUNT_BASE * ITER_RATIO`
+ */
 declare const ITER_COUNT: number;
+
+/**
+ * The final `ITER_COUNT` will multiply by this value
+ */
+declare let ITER_RATIO: number;
+
 declare const MB: number;
+
 declare const kt: (filename: string, fn: () => any) => void;
--- lib/setup.ts
+++ lib/setup.ts
@@ -1,2 +1,10 @@
-Reflect.set(globalThis, 'ITER_COUNT', 1e6);
+Reflect.set(globalThis, 'ITER_COUNT_BASE', 1e6);
+Reflect.set(globalThis, 'ITER_RATIO', 1);
 Reflect.set(globalThis, 'MB', 1024 * 1024);
+Object.defineProperty(globalThis, 'ITER_COUNT', {
+  get() {
+    return ITER_COUNT_BASE * ITER_RATIO;
+  },
+  configurable: true,
+  enumerable: true,
+});
